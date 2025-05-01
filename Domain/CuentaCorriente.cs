b2c9936aad7e31589b35f20e90c8c9fc9fb8b1fd
MartiGalind0 <Martina.GalindoLobo@alu.frt.utn.edu.ar>
2025-05-01 18:26:13 -0300

    get, set, algunas reglas

---
+++ Domain/CuentaCorriente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Ej8.Domain
+{
+    internal class CuentaCorriente
+    {
+    }
+}
