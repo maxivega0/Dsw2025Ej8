ad1fe17fe672c85dc22a197f86cf2cc447d7f5e9
Juan Lopez Asis <juan.lopezasis27@hotmail.com>
2025-05-02 03:16:21 -0300

    Se agregaron las excepciones CuentaNoActiva, MontoNoValido y SaldoInsuficiente

--- Domain/CuentaBancaria.cs
+++ Domain/CuentaBancaria.cs
@@ -1,4 +1,6 @@
-﻿namespace Dsw2025Ej8.Domain;
+﻿using Dsw2025Ej8.Exceptions;
+
+namespace Dsw2025Ej8.Domain;
 
 public abstract class CuentaBancaria
 {
---
+++ Exceptions/CuentaNoActivaException.cs
@@ -0,0 +1,14 @@
+﻿using Dsw2025Ej8.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Ej8.Exceptions
+{
+    internal class CuentaNoActivaException : Exception
+    {
+        public CuentaNoActivaException(Estado estado) : base($"No se puede operar con la cuenta {estado}") { }
+    }
+}
